a75933421875b65d07e5ed37f5bd39585770b489
Aman4017 <108785124+Aman4017@users.noreply.github.com>
2024-08-04 21:41:09 +0530

    Removed Error

--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -32,7 +32,7 @@ function FormSection({selectedTemplate, userFormInput, loading}:PROPS) {
     return (
     <div className='p-5 shadow-md border rounded-lg bg-white'>
         {/* <h2>Hello</h2> */}
-        <Image src={selectedTemplate?.icon} alt='icon' width={70} height={70}/>
+        <Image src={selectedTemplate?.icon || ''} alt='icon' width={70} height={70}/>
         <h2 className='font-bold text-2xl mb-2 text-primary'>{selectedTemplate?.name}</h2>
         <p className='text-gray-500 text-sm'>{selectedTemplate?.desc}</p>
         
